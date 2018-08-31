2b2b0627b5a9a460ded3c9053fed7892d33b658c
曾智 <service@unnnn.cn>
2018-08-31 20:01:26 +0800

    修改控制请求不到的问题。

--- unnnn-web/src/main/java/net/unnnn/web/controller/UserController.java
+++ unnnn-web/src/main/java/net/unnnn/web/controller/UserController.java
@@ -2,17 +2,24 @@ package net.unnnn.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author lx
  */
 @Controller
-@RestController("/user")
+@RequestMapping(value = "/user")
 public class UserController
 {
-	@RequestMapping("/info")
+	@RequestMapping(value = "/info", method = RequestMethod.GET)
 	public String getUser()
+	{
+		return "index";
+	}
+
+	@RequestMapping(value = "/info1", method = RequestMethod.GET)
+	public Object getUser1()
 	{
 		return "index";
 	}
